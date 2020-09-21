9fcde110c68d3ea5c5ee5b67872179c5fd9d5f2d
nguyenanh92 <50308802+nguyenanh92@users.noreply.github.com>
2020-09-21 15:56:49 +0700

    update

---
+++ Project/WizardModal/WizardModal/Controllers/PopupInfoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WizardModal.Controllers
+{
+    public class PopupInfoController : Controller
+    {
+        // GET: PopupInfo
+        public ActionResult Popup1()
+        {
+            return PartialView("_Popup1");
+        }
+        public ActionResult Popup2()
+        {
+            return PartialView("_Popup2");
+        }
+        public ActionResult Popup3()
+        {
+            return PartialView("_Popup3");
+        }
+        public ActionResult Popup4()
+        {
+            return PartialView("_Popup4");
+        }
+    }
+}
---
+++ Project/WizardModal/WizardModal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WizardModal.MvcApplication" Language="C#" %>
